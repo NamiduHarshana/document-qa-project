66cc9eae171caf4760421cde9cadcf9ed4a0695b
NamiduHarshana <151546595+NamiduHarshana@users.noreply.github.com>
2026-08-09 16:37:10 +0530

    Fix dotenv path loading for production WSGI environment

--- config/settings.py
+++ config/settings.py
@@ -11,13 +11,13 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
-load_dotenv()
-
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
